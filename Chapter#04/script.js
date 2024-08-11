4dadd6caf1785fff0d6c527fb82785c5626a95e4
MidhatJalil <MIDHATJALIL345@GMAIL.COM>
2024-08-12 02:09:57 +0500

    Add files via upload

---
+++ Chapter#04/script.js
@@ -0,0 +1,49 @@
+
+    // <!-- Problem#01
+
+    
+        // var name= "Salaar" , fName= "Yousuf", gender= "Male";
+
+    
+    // Problem#02 
+    // (legal declaration)
+    
+    //     var $name = "Haroon";
+    //     var fName = "Ikram";
+    //     var _age = 20;
+    //     var favrt_fruit = "mango";
+    //     var favrtNumber_ = 4;
+
+
+    //     // (illegal declaration)
+
+    //     var #Name= "Haroon";
+    //     var @fName = "Ikram";
+    //     var 1age = 20;
+    //     var favrt^fruit = "mango";
+    //     var FavrtNumber& = 4;
+
+    
+    
+    // Problem#03 
+
+    
+    document.write("<h2>"+"Rules for naming JS Variables"+"</h2>");
+//  (b) 
+    
+        var num= "number";
+        var char= "$";
+        var char1= "_";
+
+    document.write(" Variable names can only contain" + " "+num+", "+char+" and "+char1+". For example $my_1stVariable. <br/>" );
+    console.log(" Variable names can only contain" + " "+num+", "+char+" and "+char1+". For example $my_1stVariable. <br/>" );
+// (C)
+    var letter= "letter";
+    document.write(" Variables must begin with a "+letter+", "+char+" and "+char1+". For example $name, _name or name . <br/>");
+// (d)
+    document.write("Variable names are case sensitive.<br/>");
+    //  (e)
+    var key= "keywords";
+    document.write("Variable names should not be JS "+key+".");
+     
+     
